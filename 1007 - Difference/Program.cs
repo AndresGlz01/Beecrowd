cbc9baa18ae440a1bc6a9bb546deccf5f93a40fc
AndresGlz <andyglz01dev@gmail.com>
2022-07-27 16:46:14 -0500

    1007 - Difference

---
+++ 1007 - Difference/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1007___Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int result = (a * b) - (c * d);
+            System.Console.WriteLine($"DIFERENCA = {result}");
+        }
+    }
+}
