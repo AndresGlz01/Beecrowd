f40b86a885c2bab548a3cd7c084b5bcfb9c8043e
AndresGlz <andyglz01dev@gmail.com>
2022-07-27 16:40:12 -0500

    1006 - Average 2

---
+++ 1006 - Average 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1006___Average_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a = decimal.Parse(Console.ReadLine());
+            decimal b = decimal.Parse(Console.ReadLine());
+            decimal c = decimal.Parse(Console.ReadLine());
+
+            decimal result = ((a * 2) + (b * 3) + (c * 5)) / (2 + 3 + 5);
+            System.Console.WriteLine($"MEDIA = {result:0.0}");
+        }
+    }
+}
