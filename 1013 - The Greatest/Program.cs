d7d9e7fd5d8aafa5b98cc73fb260a9607dd49878
AndresGlz <andyglz01dev@gmail.com>
2022-08-03 20:55:18 -0500

    1013 - The Greatest

---
+++ 1013 - The Greatest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1013___The_Greatest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(values[0]);
+            int b = int.Parse(values[1]);
+            int s = int.Parse(values[2]);
+
+            int result = Compare(Compare(a, b), s);
+
+            System.Console.WriteLine("{0} eh o maior", result);
+        }
+        static int Compare(int a, int b)
+        {
+            return (a + b + Math.Abs(a - b)) / 2;
+        }
+    }
+}
