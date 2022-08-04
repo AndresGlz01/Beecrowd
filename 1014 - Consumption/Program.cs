d410f5a54daf6b9c27c6be17abfa2da0fa9c8156
AndresGlz <andyglz01dev@gmail.com>
2022-08-04 10:24:28 -0500

    1014 - 1015 - 1016

---
+++ 1014 - Consumption/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1014___Consumption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+
+            double result = x / y;
+
+            System.Console.WriteLine("{0:0.000} km/l", result);
+        }
+    }
+}
---
+++ 1015 - Distance Between Two Points/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1015___Distance_Between_Two_Points
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values1 = Console.ReadLine().Split(' ');
+            var values2 = Console.ReadLine().Split(' ');
+
+            double x1 = double.Parse(values1[0]); 
+            double y1 = double.Parse(values1[1]);
+            double x2 = double.Parse(values2[0]);
+            double y2 = double.Parse(values2[1]);
+
+            double result = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+            System.Console.WriteLine(result.ToString("0.0000"));
+        }
+    }
+}
