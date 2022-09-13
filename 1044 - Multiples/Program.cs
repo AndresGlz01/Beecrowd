a93f7e1aeb2c77069cf52abca2381eecd0e5458e
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-13 13:08:03 -0500

    1044 completed

---
+++ 1044 - Multiples/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _1044___Multiples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+
+            double value1 = double.Parse(values[0]);
+            double value2 = double.Parse(values[1]);
+
+            if (value2 % value1 == 0 || value1 % value2 == 0)
+            {
+                Console.WriteLine("Sao Multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Nao sao Multiplos");
+            }
+        }
+    }
+}
