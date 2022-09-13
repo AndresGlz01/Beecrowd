15cc7a56424941175b6e69b0f49e35381060838d
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-13 11:10:44 -0500

    1017 completed

---
+++ 1017 - Spent Fuel/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1017___Spent_Fuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int duration = int.Parse(Console.ReadLine());
+            int speed = int.Parse(Console.ReadLine());
+
+            double distance = duration * speed;
+            double result = distance / 12;
+
+            System.Console.WriteLine(result.ToString("0.000"));
+        }
+    }
+}
