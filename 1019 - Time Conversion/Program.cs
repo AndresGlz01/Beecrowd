c0d13b71640972875594d3bea245d9d6665edb61
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-13 11:48:02 -0500

    1019 completed

---
+++ 1019 - Time Conversion/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1019___Time_Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds = int.Parse(Console.ReadLine());
+
+            System.Console.Write($"{seconds / 3600}:");
+            seconds -= (seconds / 3600) * 3600;
+
+            System.Console.Write($"{seconds / 60}:");
+            seconds -= (seconds / 60) * 60;
+
+            System.Console.Write($"{seconds}");
+            System.Console.WriteLine();
+        }
+    }
+}
