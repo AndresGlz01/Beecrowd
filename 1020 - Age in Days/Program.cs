1dc47f36db3282805c0d931b3d6535afba271386
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-13 12:13:44 -0500

    1020 completed

---
+++ 1020 - Age in Days/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1020___Age_in_Days
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+
+            System.Console.WriteLine($"{age / 365} ano(s)");
+            age -= (age / 365) * 365;
+
+            System.Console.WriteLine($"{age / 30} mes(es)");
+            age -= (age / 30) * 30;
+
+            System.Console.WriteLine($"{age} dia(s)");
+        }
+    }
+}
