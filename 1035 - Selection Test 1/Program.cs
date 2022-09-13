517934d9a5d3f34e71d0add93becd6a9e91e1e75
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-13 12:28:34 -0500

    1035 completed

---
+++ 1035 - Selection Test 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1035___Selection_Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(values[0]);
+            int b = int.Parse(values[1]);
+            int c = int.Parse(values[2]);
+            int d = int.Parse(values[3]);
+
+            if(b > c & d > a & (c + d) > (a + b) & c > 0 & d > 0 & a % 2 == 0)
+            {
+                System.Console.WriteLine("Valores aceitos");
+            }
+            else
+            {
+                System.Console.WriteLine("Valores nao aceitos");
+            }
+        }
+    }
+}
