0f70a2d983fe0511b8225a4b10da9ce7dc14835a
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-14 21:26:15 -0500

    1064 completed

---
+++ 1064 - Positives and Average/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1064___Positives_and_Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = new double[6];
+            int positives = 0;
+            double average = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = double.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if(numbers[i] > 0)
+                {
+                    positives++;
+                    average += numbers[i];
+                }
+            }  
+
+            System.Console.WriteLine("{0} valores positivos", positives);   
+            System.Console.WriteLine($"{(average / positives):0.0}");   
+        }
+    }
+}
