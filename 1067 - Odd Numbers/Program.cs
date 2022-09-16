d3dfd901b568e4f7fdb007302602cbd85bdece43
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-15 23:28:53 -0500

    1067 completed

---
+++ 1067 - Odd Numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1067___Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= value; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
