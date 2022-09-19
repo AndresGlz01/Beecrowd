d07233f5c4dbe8a024af00f08592bcf33f33e2cb
CCA4 <andyglz01dev@gmail.com>
2022-09-19 15:05:21 -0500

    1070 completed

---
+++ 1070 - Six Odd Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1070___Six_Odd_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int count;
+
+            for(count = 1; count <= 6; number++)
+            {
+                if(number % 2 != 0)
+                {
+                    Console.WriteLine(number);
+                    count++;
+                }
+            }
+        }
+    }
+}
