122dbb6a97b48aa38b01cdd493c34e9da49eb519
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-23 19:32:03 -0500

    1073 completed

---
+++ 1073 - Even Square/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1073___Even_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for(int i = 2; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    System.Console.WriteLine("{0}^2 = {1}", i, i * i);
+                }
+            }
+        }
+    }
+}
