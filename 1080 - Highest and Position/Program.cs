f661d58d021bbcbc87b1a98b69d290636712b49e
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-23 19:44:39 -0500

    1080 completed

---
+++ 1080 - Highest and Position/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _1080___Highest_and_Position
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int[] numbers = new int[100];
+		int biggestNumber = 0;
+		int biggestNumberPosition = 0;
+		
+		for(int i = 0; i <= 99; i++)
+		{
+			numbers[i] = int.Parse(Console.ReadLine());
+			if(numbers[i] > biggestNumber)
+			{
+				biggestNumber = numbers[i];
+				biggestNumberPosition = i;
+			}
+		}
+		
+		
+		Console.WriteLine(biggestNumber);
+		Console.WriteLine(biggestNumberPosition + 1);
+        }
+    }
+}
