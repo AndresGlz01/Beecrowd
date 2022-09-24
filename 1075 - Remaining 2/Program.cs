53a77f3523ba2ad93fc95c41a721d2b5bc5e0c10
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-23 19:41:55 -0500

    1075 completed

---
+++ 1075 - Remaining 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1075___Remaining_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (i % number == 2)
+                {
+                    System.Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
