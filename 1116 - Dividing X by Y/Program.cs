899c0299bbd30f0e5b2ad8d02a7665a52c5eb401
Andres Gonzalez <andyglz01dev@gmail.com>
2022-09-26 21:30:44 -0500

    1116 completed

---
+++ 1116 - Dividing X by Y/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1116___Dividing_X_by_Y
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            string[] results = new string[cases];
+
+            for (int i = 0; i < cases; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                double x = int.Parse(values[0]);
+                double y = int.Parse(values[1]);
+
+                if (y == 0)
+                {
+                    results[i] = "divisao impossivel";
+                }
+                else
+                {
+                    results[i] = (x / y).ToString("0.0");
+                }
+            }
+
+            for (int i = 0; i < cases; i++)
+            {
+                Console.WriteLine(results[i]);
+            }
+        }
+    }
+}
