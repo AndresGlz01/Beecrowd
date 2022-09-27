34e9ccb44c606c86b27ddb435f0389b3ff17804d
AlumnoCCA1 <alumnocca1>
2022-09-27 13:33:27 -0500

    1387 completed

---
+++ 1387 - Og/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1387___Og
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = 0;
+            int r = 0;
+            List<int> results = new List<int>();
+
+            do
+            {
+                string[] values = Console.ReadLine().Split(' ');
+
+                l = int.Parse(values[0]);
+                r = int.Parse(values[1]);
+
+                if(l == 0 && r == 0)
+                {
+                    break;
+                }
+
+                results.Add(l + r);
+
+            } while (l != 0 && r != 0);
+
+            results.ForEach(x => System.Console.WriteLine(x));
+        }
+    }
+}
