8b8ced9c102e06f48fab0df72e6d79288da6f5f6
Andres Gonzalez <andyglz01dev@gmail.com>
2022-10-04 22:08:50 -0500

    1936 completed

---
+++ 1936 - Factorial/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1936___Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int count = 0;
+            int temp = 0;
+            int f = 0;
+
+            for (int i = 8; i >= 0; --i)
+            {
+                f = fat(i);
+                temp = number / f;
+                number -= temp * f;
+                count += temp;
+            }
+
+            Console.WriteLine(count);
+        }
+
+        static int fat(int n)
+        {
+            if (n <= 1) return 1;
+            else return n * fat(n - 1);
+        }
+    }
+}
