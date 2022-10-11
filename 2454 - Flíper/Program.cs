97c1eadcedf3cc05b3d8d55aae603c64a80f95fb
Andres Gonzalez <andyglz01dev@gmail.com>
2022-10-10 21:20:47 -0500

    2454 completed

---
+++ 2454 - Flíper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2454___Flíper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] values = Console.ReadLine().Split();
+
+            int P = int.Parse(values[0]);
+            int R = int.Parse(values[1]);
+
+            if (P == 0)
+            {
+                System.Console.WriteLine("C");
+                return;
+            }
+
+            if (R == 0)
+            {
+                System.Console.WriteLine("B");
+                return;
+            }
+
+            System.Console.WriteLine("A");
+        }
+    }
+}
