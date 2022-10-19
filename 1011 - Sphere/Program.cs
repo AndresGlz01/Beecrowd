08f05f49255735be0915d830609e1eadf5fb2272
Andres Gonzalez <andyglz01dev@gmail.com>
2022-10-19 10:49:59 -0500

    fixed framework

--- 1011 - Sphere/Program.cs
+++ 1011 - Sphere/Program.cs
@@ -1,3 +1,14 @@
-﻿int rad = int.Parse(Console.ReadLine());
-double result = (4d / 3d) * 3.14159d * Math.Pow(rad, 3);
-System.Console.WriteLine("VOLUME = {0:0.000}", result);
+﻿using System;
+
+namespace _1011___Sphere
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rad = int.Parse(Console.ReadLine());
+            double result = (4d / 3d) * 3.14159d * Math.Pow(rad, 3);
+            System.Console.WriteLine("VOLUME = {0:0.000}", result);
+        }
+    }
+}
